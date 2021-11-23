afa7690e588bc1db70f56d2998b96b5bc69bce67
Samia Saeed <saminasaeed@MP-AhmadB-CSM-365.local>
2021-11-23 14:25:34 +0500

    done cron jobs through nodemailer

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const path = require("path");
 const swaggerUi = require("swagger-ui-express");
 const Joi = require("joi");
+const cron = require("node-cron");
 const routes = require("./routes/user");
 const swaggerDocument = require("./swagger.json");
+const job = require("./cron-jobs");
 // Database
 const db = require("./config/database");
 
@@ -11,6 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
+
+// cron.schedule("* * * * *", () => {
+//   console.log(`Task is running every minute ${new Date()}`);
+// });
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
